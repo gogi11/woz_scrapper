bb6180c96c3c3fb56f2a93dcbcea1bd7cb01115e
Georgi Kostov <kostov.georgi11@gmail.com>
2019-09-11 13:22:55 +0200

    Adding export to json

--- run.py
+++ run.py
@@ -13,8 +13,12 @@ objects_done = 0
 objects_total = 0
 
 
-def parse_json_save_to_sqlite(json_string):
-    
+def parse_json_save_to_sqlite(json_string, coords, id):
+    post = id[0]
+    hnr = id[1]
+    long = coords["x"]
+    lat = coords["y"]
+
     json_obj = None
 
     try:
@@ -22,167 +26,36 @@ def parse_json_save_to_sqlite(json_string):
     except Exception as e:
         logging.error(e)
         logging.error("there is some problem with json loading")
-    
-    total = None
-    try:
-        total = len(json_obj['features'])
-    except Exception as e:
-        logging.error(e)
-        logging.error("There is no features here")
-    
-    obj_features = []
-    try:
-        obj_features = json_obj['features']
-    except Exception as e:
-        logging.error(e)
-        logging.error("There is no features here")
+
+    building = {
+        "house_number" : hnr,
+        "postcode" : post,
+        "longitude": long,
+        "latitude": lat
+    }
 
     try:
-        if int(total) > 0:
-            print("------------- get total: %s properties ----------------" % str(total))
-        else:
-            pass
-    except Exception as e:
-        pass
+        with open('./exported_data.csv', 'a+') as export:
+            export.write(str(hnr) + "," + str(post) + "," + str(long) + "," + str(lat)+"\n")
+    except:
+        print("There was an error with saving to db.")
 
-    for index, each_building in enumerate(obj_features):
-        building_info = None
-        try:
-            building_info = each_building['properties']
-        except Exception as e:
-            logging.error(e)
-            logging.error("There is no properties here")
-        
-        building = PropertyModel()
-        if len(str(building_info['wobj_obj_id'])) > 0:
-            try:
-                building.identificatie = building_info['wobj_obj_id']
-            except Exception as e:
-                print("this building has no identification code. ")
-                building.identificatie = "none"
-        else:
-            building.identificatie = "none"
-            print("this building has no identification code. ")
-            
-        if len(str(building_info['wobj_huisnummer'])) > 0:
-            try:
-                building.house_number = building_info['wobj_huisnummer']
-            except Exception as e:
-                building.house_number = "none"
-                print("this building has no house number. ")
-        else:
-            building.house_number = "none"
-            print("this building has no house number. ")
-        
-        if len(str(building_info['wobj_huisletter'])) > 0:
-            try:
-                building.house_number_ext = building_info['wobj_huisletter']
-            except Exception as e:
-                building.house_number_ext = "none"
-                print("this building has no house number extension")
-        else:
-            building.house_number_ext = "none"
-            print("this building has no house number extension")
-            
-        if len(str(building_info['wobj_postcode'])) > 0:
-            try:
-                building.postcode = building_info['wobj_postcode']
-            except Exception as e:
-                building.postcode = "none"
-                print('this building has no postcode. ')
-        else:
-            building.postcode = "none"
-            print('this building has no postcode. ')
-            
-        if len(str(building_info['wobj_woonplaats'])) > 0:
-            try:
-                building.plaatsnaam = building_info['wobj_woonplaats']
-            except Exception as e:
-                building.plaatsnaam = "none"
-                print("this building has no plaatsnaam. ")
-        else:
-            building.plaatsnaam = "none"
-            print("this building has no plaatsnaam. ")
-        
-        if len(str(building_info['wobj_straat'])) > 0:
-            try:
-                building.street = building_info['wobj_straat']
-            except Exception as e:
-                building.street = "none"
-                print("this building has no street name. ")
-        else:
-            building.street = "none"
-            print("this building has no street name. ")
-            
-        if len(str(building_info['wobj_bag_bouwjaar'])) > 0:
-            try:
-                building.bouwjaar = building_info['wobj_bag_bouwjaar']
-            except Exception as e:
-                building.bouwjaar = "none"
-                print("this building has no bouwjaar. ")
-        else:
-            building.bouwjaar = "none"
-            print("this building has no bouwjaar. ")
-            
-        if len(str(building_info['wobj_bag_gebruiksdoel'])) > 0:
-            try:
-                building.gebruiksdoel = building_info['wobj_bag_gebruiksdoel']
-            except Exception as e:
-                building.gebruiksdoel = "none"
-                print("this building has no gebruiksdoel. ")
-        else:
-            building.gebruiksdoel = "none"
-            print("this building has no gebruiksdoel. ")
-            
-        if len(str(building_info['wobj_oppervlakte'])) > 0:
-            try:
-                building.oppervlakte = building_info['wobj_oppervlakte']
-            except Exception as e:
-                building.oppervlakte = "none"
-                print("this building has no oppervlakte. ")
-        else:
-            building.oppervlakte = "none"
-            print("this building has no oppervlakte. ")
-
-        pr_2015, pr_2016, pr_2017 = \
-            parse_each_property_price(scrape_each_property_price(building.identificatie))
-
-        if pr_2015 is not None:
-            building.price_2015 = pr_2015
-
-        if pr_2016 is not None:
-            building.price_2016 = pr_2016
-
-        if pr_2017 is not None:
-            building.price_2017 = pr_2017
-        
-        try:
-            building.save()
-        except Exception as e:
-            logging.error(e)
-            logging.error("Got a duplicate record")
-        
-#         print("Saving property to database now, process: {:2.2f}% ...".format((index+1)*100/(total)), end="\r")
 
 
 class BaseModel(Model):
     class Meta:
         database = SqliteDatabase("netherland_properties.db")
 class PropertyModel(BaseModel):
-    identificatie = CharField(unique=True)
     house_number = CharField(null=True)
-    house_number_ext = CharField(null=True)
     postcode = CharField(null=True)
-    plaatsnaam = CharField(null=True)
-    street = CharField(null=True)
     
     price_2015 = CharField(null=True)
     price_2016 = CharField(null=True)
     price_2017 = CharField(null=True)
-    
-    bouwjaar = CharField(null=True)
-    gebruiksdoel = CharField(null=True)
-    oppervlakte = CharField(null=True)
+    price_2018 = CharField(null=True)
+
+    longitude = CharField(null=True)
+    latitude = CharField(null=True)
 
 def init_database():
     db = SqliteDatabase("netherland_properties.db")
@@ -195,15 +68,12 @@ def init_database():
     db.close()
 
 
-def scrape_obj_from_id_to_id(f=None, t=None):
+def scrape_obj_from_id_to_id(f=None):
     if f is None:
         raise Exception("From id is None, check the script please. ")
-    if t is None:
-        raise Exception("To id is None, check the script please. ")
-        
-    # format from_id and to_id
+
+    # format from_id
     from_id = '%012d'%f
-    to_id = '%012d'%t
     
     # initiate a request object
     s = requests.Session()
@@ -225,62 +95,67 @@ def scrape_obj_from_id_to_id(f=None, t=None):
                 <ogc:And>
                     <ogc:PropertyIsEqualTo matchCase="true">
                         <ogc:PropertyName>wobj_bag_obj_id</ogc:PropertyName>
-                        <ogc:Literal>772010000791852</ogc:Literal>
+                        <ogc:Literal>%s</ogc:Literal>
                     </ogc:PropertyIsEqualTo>
                 </ogc:And>
             </ogc:Filter>
         </wfs:Query>
     </wfs:GetFeature>
     """
-    #xml_obj = xml_obj%(str(from_id),)
+    xml_obj = xml_obj%(str(from_id),)
     
     response = None
     try:
         response = s.post(url="https://www.wozwaardeloket.nl/woz-proxy/wozloket", data=xml_obj)
-        print(response.json())
-        print("scraping woz obj from id %s to id %s . "%(str(from_id), str(to_id)), end="\n")
+        # print(response.json())
+        print("scraping woz obj with id "+str(from_id)+". \n")
     except Exception as e:
         logging.error(e)
         logging.error("request has met problem. ")
         logging.error("from_id=%s"%str(from_id))
-        logging.error("to_id=%s"%str(to_id))
 
     text_response = None
     try:
-    	text_response = response.text
+        text_response = response.text
     except:
-    	text_response = None
+        text_response = None
         
     return text_response
 
 
-def scrape_range_and_save(arg):
+def scrape_range_and_save(array_with_post_and_hnr):
     global objects_total, objects_done
-    step = 5000
-    json_string = scrape_obj_from_id_to_id(arg*step+1, ((arg+1)*step)+2)
-    parse_json_save_to_sqlite(json_string=json_string)
+    post = array_with_post_and_hnr[0]
+    hnr = array_with_post_and_hnr[1]
+
+    r = requests.get('https://www.wozwaardeloket.nl/api/geocoder/v3/suggest?query='+post+"%20"+hnr)
+    id = r.json()["docs"][0]["id"]
+    r = requests.get('https://www.wozwaardeloket.nl/api/geocoder/v3/lookup?id='+id)
+    r_json = r.json()
+    bag_obj_id = r_json['adresseerbaarobject_id']
+    coords = r_json['centroide_ll']
+    json_string = scrape_obj_from_id_to_id(int(bag_obj_id))
+    parse_json_save_to_sqlite(json_string=json_string, coords=coords, id=array_with_post_and_hnr)
     objects_done += 1
     print("----------------- Process: {:2.2f}%------------------".format((objects_done)*100/(objects_total)))
 
 
-
-def stage1_scrape_all_obj():
+def stage1_scrape_all_obj(array_with_post_and_house):
     global objects_total, objects_done
 
-    threads = input("how many threads you want to use? (e.g. 100)")
-    #computers = input("how many crawlers do you want to deploy in total? (e.g. 2)")
-    #index_computer = input("please type in the index of this computer (e.g. 0)")
+    # threads = input("how many threads you want to use? (e.g. 100)")
 
     print("deploying threads, please wait .... ")
-    total_steps = range(0, 1)
+    total_steps = range(len(array_with_post_and_house))
     objects_total = len(total_steps)
 
-    chunks = [total_steps[x:x + 1000] for x in range(0, len(total_steps), 1000)]
-    for each_chunk in chunks:
-        pool = ThreadPool(int(threads))
-        pool.map(scrape_range_and_save, each_chunk)
-        pool.close()
-        pool.join()
+    #pool = ThreadPool(int(threads))
+
+    for entry in array_with_post_and_house:
+        scrape_range_and_save(entry)
+        # pool.map(scrape_range_and_save, entry)
+        # pool.close()
+        # pool.join()
 
     objects_total = 0
     objects_done = 0
@@ -335,8 +210,6 @@ def scrape_each_property_price(property_id):
     return t_response
 
 
-
-
 def parse_each_property_price(json_string):
     json_objs = []
     try:
@@ -375,8 +248,11 @@ def parse_each_property_price(json_string):
     return price15, price16, price17
 
 
-
 if __name__ == '__main__':
     init_database()
-    stage1_scrape_all_obj()
+    stage1_scrape_all_obj([
+                            ["5623PC", "314"],
+                            ["5623PC", "312"],
+                            ["6214AT", "22"]
+    ])
     print("script finished :) ")
